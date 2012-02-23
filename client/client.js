d7535b20b1f600ea62cf3f337cd1eca3c947dd8a
Viktor Kronvall <viktor.kronvall@gmail.com>
2012-02-23 23:45:02 +0900

    Starting to change to elasticalsearch

--- client/client.js
+++ client/client.js
@@ -1,23 +1,34 @@
-(function() {
-  jQuery(function($) {
-    $("#searchForm").submit(function(event) {
+(function () {
+  jQuery(function ($) {
+    $("#searchForm").submit(function (event) {
       event.preventDefault();
       var searchValue = document.getElementById("searchbar").value;
       window.location.href = "/search/" + encodeURIComponent(searchValue);
     });
-		$("#loadMore").click(function(event) {
+    $(window).scroll(function () {
+      if ($(window).scrollTop() >= $(document).height() - $(window).height() - 100) {
+        //Add something at the end of the page
+        var moreButton = $("#loadMore");
+        loadMore(moreButton);
+      }
+    });
+    $("#loadMore").click(function (event) {
       event.preventDefault();
       var moreButton = $(this);
-      var fromindex = moreButton.attr("data-from");
-			$.ajax({
-			  url: "/blog/more/"+fromindex,
-			  context: $("#content .posts"),
-			  success: function(data){
-			    this.append(data.html);
-			    moreButton.attr("data-from",data.fromindex);
-			  }
-			});
+      loadMore(moreButton);
     });
-    
+
+    function loadMore(anchor) {
+      var fromindex = anchor.attr("data-from");
+      $.ajax({
+        url: "/blog/more/" + fromindex,
+        context: $("#content .posts"),
+        success: function (data) {
+          this.append(data.html);
+          anchor.attr("data-from", data.fromindex);
+        }
+      });
+    }
+
   });
 })();
