05dfd8526dac8414275a897a5509cd29519f1bb4
Viktor Kronvall <viktor.kronvall@gmail.com>
2012-02-24 02:29:55 +0900

    Starting to change to elasticalsearch

---
+++ setupelastic.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+server="localhost"
+port="9997"
+
+# Create the index
+curl -XPUT    "$server:$port/gogoindex"
+
+# Create the mapping
+curl -XPUT    "$server:$port/gogoindex/gogotype/_mapping" -d '
+{
+    "gogotype" : {
+        "properties" : {
+            "type" : {"type" : "string", "index":"not_analyzed"}
+        }
+    }
+}
+'
+
+# Configure the river
+curl -XPUT "$server:$port/_river/gogoindex/_meta" -d '{
+    "type" : "couchdb",
+    "couchdb" : {
+        "host" : "localhost",
+        "port" : 5984,
+        "db" : "gogoblog",
+        "filter":null
+    },
+    "index" : {
+        "index" : "gogoindex",
+        "type" : "gogotype"
+    }
+}'
