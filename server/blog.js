70b21faf3f9d8fabab4093f95c5a2d04b19169b9
Viktor Kronvall <viktor.kronvall@gmail.com>
2012-02-24 02:29:25 +0900

    Starting to change to elasticalsearch

--- server/blog.js
+++ server/blog.js
@@ -115,7 +115,7 @@ function getPosts(blog, options, callback) {
     viewName = "blog/listall";
   }
   var startindex = options.from || 0;
-  var size = options.size || 2;
+  var size = options.size || 3;
   var endindex = startindex+size;
 
   client.search({
