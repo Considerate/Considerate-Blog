60c4f5745b14dbf684879cbac6617fe945c88663
Viktor Kronvall <viktor.kronvall@gmail.com>
2012-02-23 22:34:07 +0900

    Starting to change to elasticalsearch

--- server/blog.js
+++ server/blog.js
@@ -122,7 +122,7 @@ function getPosts(blog, options, callback) {
       "match_all": {}
     }
   }, function (err, results, res) {
-    require("util").inspect(results);
+    console.log(require("util").inspect(results));
   });
 
   db.view(viewName, options, function (err, res) {
