f5fd9ac2d0fe2097e1315aae9192f21632e04ae7
Viktor Kronvall <viktor.kronvall@gmail.com>
2012-02-24 02:26:01 +0900

    Starting to change to elasticalsearch

--- client/client.js
+++ client/client.js
@@ -33,7 +33,7 @@
         url: "/blog/more/" + fromindex,
         context: $("#content .posts"),
         success: function (data) {
-          this.append(data.html);
+          $(data.html).appendTo(this).slideDown();
           anchor.attr("data-from", data.fromindex);
           callback();
         }
