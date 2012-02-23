4131a240c5360a8ff12e65d2fa7a3137ecbc9b80
Viktor Kronvall <viktor.kronvall@gmail.com>
2012-02-24 03:29:51 +0900

    Starting to change to elasticalsearch

--- client/client.js
+++ client/client.js
@@ -6,24 +6,26 @@
       window.location.href = "/blog/search/" + encodeURIComponent(searchValue);
     });
     var timer;
-    $(window).scroll(function () {
-      if ($(window).scrollTop() >= $(document).height() - $(window).height() - 100) {
-        if (!timer) {
-          timer = setTimeout(function () {
-            //Add something at the end of the page
-            var moreButton = $("#loadMore");
-            loadMore(moreButton, function () {
-              timer = null;
-            });
-          }, 400);
+
+    var moreButton = $("#loadMore");
+    if (moreButton) {
+      $(window).scroll(function () {
+        if ($(window).scrollTop() >= $(document).height() - $(window).height() - 100) {
+          if (!timer) {
+            timer = setTimeout(function () {
+              //Add something at the end of the page
+              loadMore(moreButton, function () {
+                timer = null;
+              });
+            }, 400);
+          }
         }
-      }
-    });
-    $("#loadMore").click(function (event) {
-      event.preventDefault();
-      var moreButton = $(this);
-      loadMore(moreButton);
-    });
+      });
+      moreButton.click(function (event) {
+        event.preventDefault();
+        loadMore(moreButton);
+      });
+    }
 
     function loadMore(anchor, callback) {
       var fromindex = anchor.attr("data-from");
