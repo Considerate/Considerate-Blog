d6bd66464be8f7a14e098a707f7ad2d53f851e0f
Viktor Kronvall <viktor.kronvall@gmail.com>
2012-02-23 23:29:19 +0900

    Starting to change to elasticalsearch

--- client/client.js
+++ client/client.js
@@ -7,13 +7,14 @@
     });
 		$("#loadMore").click(function(event) {
       event.preventDefault();
-      
-      var fromindex = $(this).attr("data-from");
+      var moreButton = $(this);
+      var fromindex = moreButton.attr("data-from");
 			$.ajax({
 			  url: "/blog/more/"+fromindex,
 			  context: $("#content .posts"),
 			  success: function(data){
 			    this.append(data.html);
+			    moreButton.attr("data-from",data.fromindex);
 			  }
 			});
     });
