c1afe2856387dddadbfec8d061b05bbfedc6531d
Viktor Kronvall <viktor.kronvall@gmail.com>
2012-02-24 09:15:43 +0900

    Starting to change to elasticalsearch

--- server/server.js
+++ server/server.js
@@ -66,7 +66,7 @@ app.post('/login', function (req, res) {
 });
 
 app.get('/logout', function (req, res) {
-  req.session.user = null;
+  req.session.user = undefined;
   res.redirect('/login');
 });
 
