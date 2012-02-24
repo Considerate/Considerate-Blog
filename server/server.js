e425397e3f3e8361ba27eff21ae4f996c1c6b9ff
Viktor Kronvall <viktor.kronvall@gmail.com>
2012-02-24 09:04:08 +0900

    Starting to change to elasticalsearch

--- server/server.js
+++ server/server.js
@@ -146,7 +146,8 @@ app.get("/blog", function (req, res) {
 
 app.get("/blog/more/:fromindex", function (req, res) {
   var options = {
-    from: Number(req.params.fromindex)
+    from: Number(req.params.fromindex),
+    language: req.params.language
   };
   blogEngine.getPosts(blog, options, function (posts, endindex) {
     var renderData = blog;
